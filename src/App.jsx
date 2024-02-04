49809c749c9c871cd55c5a4de759d8a0081a17a2
symonmuchemi <simonkenya846@gmail.com>
2024-02-04 20:09:23 +0300

    pizza component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
+import Pizza from './components/pizza'
 
+const Header = () => {
+  return <h2>Fast React Pizza Co.</h2>
+}
+
+const Menu = () => {
+  return <h3>Menu</h3>
+}
+
+const Footer = () => {
+  let time = new Date().toLocaleTimeString();
+
+  return <footer>{time}. We're currently open! </footer>
+}
 function App() {
 
-  return (
-    <>
-      <p>Hello world</p>
-    </>
-  )
+  return <div>
+    <Header/>
+    <Menu/>
+    <Pizza/>
+    <Footer/>
+  </div>
 }
 
 export default App
---
+++ src/components/pizza.jsx
@@ -0,0 +1,10 @@
+function Pizza() {
+
+    return <div>
+        <img src="/pizzas/spinaci.jpg" alt="pizza spinaci"></img>
+        <h2>Pizza Spinaci</h2>
+        <p>Tomato, mozarela, spinach and ricotta cheese</p>
+    </div>
+}
+
+export default Pizza
